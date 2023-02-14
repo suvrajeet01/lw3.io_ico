2aab9c8dae8f99d43048a7ce0ca189deca367501
suvrajeet01 <suvrajeetbanerjee34767@gmail.com>
2023-02-14 17:05:43 +0530

    ico

---
+++ ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x4Ae2a6DA43907593859A9fE62ce3c71a7189AD6a";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
